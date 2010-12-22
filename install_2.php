a2ddf989d467a384d7e8c767cc1f1f153da9ee51
Frans de Jonge <fransdejonge@gmail.com>
2010-12-23 00:00:00 +0100

    Stop Spammer v2.3.8 23 December 2010
    Version 2.3.8 - 23th December 2010
     * Changes in the Code to make the mod compatible with 2.0 RC4
       (thanks to busterone here: http://www.simplemachines.org/community/index.php?topic=283309.msg2837547#msg2837547)
     ! Fixed the bug about "Undefined index: types"
       (thanks to gizmo71 here: http://www.simplemachines.org/community/index.php?topic=283309.msg2800431#msg2800431)
     - Eliminated completely the default API key as requested by
       Stop Forum Spam's admin (see here: http://www.simplemachines.org/community/index.php?topic=283309.msg2853286#msg2853286)
     ? Unistall & Update. It's not necessary to re-edit the template files
       unless you have in your template folder the file
       ManageMembers.template.php or Register.template.php

--- install_2.php
+++ install_2.php
@@ -2,7 +2,7 @@
 /*
 	<id>M-DVD:StopSpammer</id>
 	<name>Stop Spammer</name>
-	<version>2.3.7</version>
+	<version>2.3.8</version>
 */
 global $smcFunc, $db_prefix;
 
@@ -27,15 +27,29 @@ $smcFunc['db_insert']('ignore',
 			array('variable' => 'string','value' => 'string'),
 			array(
 				array ('stopspammer_count' ,'0'),
-				array ('stopspammer_enable','1'),
+				array ('stopspammer_enable','0'),
 				array ('stopspammer_show01','1'),
 				array ('stopspammer_faildb','2'),
 				array ('stopspammer_api_key',''),
-				array ('stopspammer_check_name','1'),
+				array ('stopspammer_check_name','0'),
 				array ('stopspammer_check_mail','1'),
 				array ('stopspammer_check_ip','1')
 			),
 			array()
 		);
+
+// This mod cannot be enabled without an API key
+if (isset($modSettings['stopspammer_enable']) && 1 == intval($modSettings['stopspammer_enable']) && isset($modSettings['stopspammer_api_key']) && $modSettings['stopspammer_api_key'] == '')
+{
+	$smcFunc['db_query']('', '
+		UPDATE {db_prefix}settings
+		SET value = {string:value}
+		WHERE variable = {string:variable}',
+		array(
+			'value' => '0',
+			'variable' => 'stopspammer_enable',
+		)
+	);
+}
 
 ?>
