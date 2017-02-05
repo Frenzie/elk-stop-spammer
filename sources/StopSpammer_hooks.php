b139024e6a15c11368f520020d754435a91f532d
Frans de Jonge <fransdejonge@gmail.com>
2017-02-05 12:22:46 +0100

    Added hooks file

---
+++ sources/StopSpammer_hooks.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @author  Frans de Jonge http://fransdejonge.com
+ * @license GPLv3
+ * @mod     Stop Spammer
+ *
+ */
+
+if (!defined('ELK'))
+	die('No access...');
+
+class StopSpammer_integrate
+{
+	public static function modify_registration_settings(&$config_vars)
+	{
+		
+	}
+}
+?>
