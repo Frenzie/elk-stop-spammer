6b70c1b0a5042a5c5ccae963c12db8d1c150fa4c
Frans de Jonge <fransdejonge@gmail.com>
2017-02-06 15:44:08 +0100

    Added source mod =(

--- sources/StopSpammer_hooks.php
+++ sources/StopSpammer_hooks.php
@@ -14,6 +14,21 @@ if (!defined('ELK'))
 class StopSpammer_integrate
 {
 
+	// Add link to Stop Spammer settings (under registration settings) to add-on settings
+	public static function general_mod_settings(&$config_vars)
+	{
+		global $txt; // need $txt 'cause loadLanguage only fills in the setting's name
+		loadLanguage('StopSpammer');
+
+		$add = array(
+				'',
+			array('title', 'stopspammer_settings'),
+			'<a href="?action=admin;area=regcenter;sa=settings#stopspammer_enable">' . $txt['stopspammer_enable'] . '</a> [' . $txt['admin_search_section_settings'] . ']'
+		);
+		$config_vars = array_merge($config_vars, $add);
+	}
+
+
 /*
 	<file name="SOURCEDIR/ManageMembers.php">
 		<!--- Load our functions at the beginning of ViewMembers() for every sa we need --->
@@ -297,45 +312,9 @@ class StopSpammer_integrate
 		};
 	}
 
+	// Add Stop Spammer settings under registration settings
 	public static function modify_registration_settings(&$config_vars)
 	{
-
-/*
- * 
-	<file name="SOURCEDIR/ManageRegistration.php">
-		<operation>
-			<search position="before"><![CDATA[
-	require_once($sourcedir . '/ManageServer.php');
-]]></search>
-			<add><![CDATA[
-	// Test if mod StopSpammer is OK
-	require_once($sourcedir . '/StopSpammer.php');
-	$txt['stopspammer_ok'] = stopspammer_test_mod_ok();
-	$stopspammer_faildb_sub = $txt['stopspammer_faildb_sub'];
-]]></add>
-		</operation>
-
-		<operation>
-			<search position="before"><![CDATA[
-			array('text', 'coppaPhone'),]]></search>
-			<add><![CDATA[
-		// Stop Spammer
-		array('title', 'stopspammer_settings'),
-		$txt['stopspammer_ok'],
-		'',
-			array('check', 'stopspammer_enable', 'subtext' => $txt['stopspammer_enable_sub']),
-		array('desc', 'stopspammer_check_sub1'),
-			array('check', 'stopspammer_check_name'),
-			array('check', 'stopspammer_check_mail'),
-			array('check', 'stopspammer_check_ip'),
-		array('desc', 'stopspammer_check_sub2'),
-			array('select', 'stopspammer_faildb', array($txt['stopspammer_fail01'], $txt['stopspammer_fail02'], $txt['stopspammer_fail03']), 'subtext' => $stopspammer_faildb_sub),
-			array('check', 'stopspammer_show01', 'subtext' => $txt['stopspammer_show01_sub']),
-			array('text', 'stopspammer_api_key'),
-		array('desc', 'stopspammer_api_key_sub'),]]></add>
-		</operation>
-	</file>
-*/
 
 		global $txt; // need $txt 'cause loadLanguage only fills in the setting's name
 		loadLanguage('StopSpammer');
@@ -362,7 +341,7 @@ class StopSpammer_integrate
 				array('text', 'stopspammer_api_key'),
 			array('desc', 'stopspammer_api_key_sub'),
 		);
-		$config_vars = elk_array_insert($config_vars, 4, $add);
+		$config_vars = array_merge($config_vars, $add);
 	}
 
 
